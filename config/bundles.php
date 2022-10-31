9701efa39d4d1f2d7c0fb88f0739442ede958291
weijiangyang <weijiangyang@laposte.net>
2022-10-31 19:24:53 +0100

    fixture10

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
