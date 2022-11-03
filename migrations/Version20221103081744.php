b1168eea4d6e7e811fcb5d4a72bc29cd845de6f7
weijiangyang <weijiangyang@laposte.net>
2022-11-03 09:20:24 +0100

    imagename

---
+++ migrations/Version20221103081744.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221103081744 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient CHANGE image_name image_name VARCHAR(255) ');
+        $this->addSql('ALTER TABLE recipe CHANGE image_name image_name VARCHAR(255)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient CHANGE image_name image_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE recipe CHANGE image_name image_name VARCHAR(255) DEFAULT NULL');
+    }
+}
