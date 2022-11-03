928dff831f4c6e06056355ee3c25e4f0925429d8
weijiangyang <weijiangyang@laposte.net>
2022-11-03 09:14:31 +0100

    imagename

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -44,7 +44,7 @@ class Ingredient
     #[Vich\UploadableField(mapping: 'recipe_images', fileNameProperty: 'imageName')]
     private ?File $imageFile = null;
 
-    #[ORM\Column(type: 'string', nullable: true)]
+    #[ORM\Column(type: 'string', nullable: false)]
     private ?string $imageName = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -80,7 +80,7 @@ class Recipe
     #[Vich\UploadableField(mapping: 'recipe_images', fileNameProperty: 'imageName')]
     private ?File $imageFile = null;
 
-    #[ORM\Column(type: 'string', nullable: true)]
+    #[ORM\Column(type: 'string', nullable: false)]
     private ?string $imageName = null;
 
     #[ORM\OneToMany(mappedBy: 'recipe', targetEntity: Comment::class, orphanRemoval: true)]
