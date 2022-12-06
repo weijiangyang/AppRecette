18b9f5e963643c18a2022891cdfaf373231912af
weijiangyang <weijiangyang@laposte.net>
2022-12-06 14:29:49 +0100

    ajout admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -36,95 +36,102 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         //user
-        $users = [];
-        for ($k = 0; $k < 10; $k++) {
-            $user = new User;
-            $user->setEmail($this->faker->email())
-                ->setRoles(['ROLE_USER'])
-                ->setPlainPassword('password')
-                ->setFullName($this->faker->name())
-                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null);
-            $manager->persist($user);
-            $users[] = $user;
-        }
-        // ingredients
-        $ingredients = [];
-        for ($i = 0; $i < 50; $i++) {
-            $ingredient = new Ingredient;
-            $ingredient->setName($this->faker->word())
-                ->setPrice(mt_rand(0, 1) === 1 ? mt_rand(1, 150) : 0)
-                ->setUser($users[mt_rand(0, count($users) - 1)])
-                ->setUnit($this->faker->word())
-                ->setSlug((new Slugify())->slugify($ingredient->getName()) . '-' . uniqid())
-                ->setImageName(null);
-            $manager->persist($ingredient);
-            $ingredients[] = $ingredient;
-        }
+        $admin = new User;
+        $admin->setEmail('admin@gmail.com');
+        $admin->setFullName('admin')
+            ->setPseudo('admin')
+            ->setRoles(["ROLE_USER","ROLE_ADMIN"])
+            ->setPlainPassword('123');
+        $manager -> persist($admin);
+        // $users = [];
+        // for ($k = 0; $k < 10; $k++) {
+        //     $user = new User;
+        //     $user->setEmail($this->faker->email())
+        //         ->setRoles(['ROLE_USER'])
+        //         ->setPlainPassword('password')
+        //         ->setFullName($this->faker->name())
+        //         ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null);
+        //     $manager->persist($user);
+        //     $users[] = $user;
+        // }
+        // // ingredients
+        // $ingredients = [];
+        // for ($i = 0; $i < 50; $i++) {
+        //     $ingredient = new Ingredient;
+        //     $ingredient->setName($this->faker->word())
+        //         ->setPrice(mt_rand(0, 1) === 1 ? mt_rand(1, 150) : 0)
+        //         ->setUser($users[mt_rand(0, count($users) - 1)])
+        //         ->setUnit($this->faker->word())
+        //         ->setSlug((new Slugify())->slugify($ingredient->getName()) . '-' . uniqid())
+        //         ->setImageName(null);
+        //     $manager->persist($ingredient);
+        //     $ingredients[] = $ingredient;
+        // }
 
-        //categories
-        $categories = [];
-        $category = new Category;
-        $category->setName('chinoise');
-        $manager->persist($category);
-        $categories[] = $category;
+        // //categories
+        // $categories = [];
+        // $category = new Category;
+        // $category->setName('chinoise');
+        // $manager->persist($category);
+        // $categories[] = $category;
 
-        $category = new Category;
-        $category->setName('française');
-        $manager->persist($category);
-        $categories[] = $category;
+        // $category = new Category;
+        // $category->setName('française');
+        // $manager->persist($category);
+        // $categories[] = $category;
 
 
-        //recipes:
-        $recipes = [];
-        for ($i = 0; $i < 25; $i++) {
-            $recipe = new Recipe;
-            $recipe->setName($this->faker->word())
-                ->setTime(mt_rand(0, 1) === 1 ? mt_rand(1, 1440) : null)
-                ->setNbPeople(mt_rand(0, 1) === 1 ? mt_rand(1, 50) : null)
-                ->setDifficulty(mt_rand(0, 1) === 1 ? mt_rand(1, 5) : null)
-                ->setDescription($this->faker->text(300))
-                ->addCategory($categories[mt_rand(0, count($categories) - 1)])
-                ->setPrice(mt_rand(0, 1) === 1 ? mt_rand(1, 1000) : null)
-                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false)
-                ->setIsPublic(true)
-                ->setSlug((new Slugify())->slugify($recipe->getName()) . '-' . uniqid())
-                ->setUser($users[mt_rand(0, count($users) - 1)]);
+        // //recipes:
+        // $recipes = [];
+        // for ($i = 0; $i < 25; $i++) {
+        //     $recipe = new Recipe;
+        //     $recipe->setName($this->faker->word())
+        //         ->setTime(mt_rand(0, 1) === 1 ? mt_rand(1, 1440) : null)
+        //         ->setNbPeople(mt_rand(0, 1) === 1 ? mt_rand(1, 50) : null)
+        //         ->setDifficulty(mt_rand(0, 1) === 1 ? mt_rand(1, 5) : null)
+        //         ->setDescription($this->faker->text(300))
+        //         ->addCategory($categories[mt_rand(0, count($categories) - 1)])
+        //         ->setPrice(mt_rand(0, 1) === 1 ? mt_rand(1, 1000) : null)
+        //         ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false)
+        //         ->setIsPublic(true)
+        //         ->setSlug((new Slugify())->slugify($recipe->getName()) . '-' . uniqid())
+        //         ->setUser($users[mt_rand(0, count($users) - 1)]);
 
-            for ($k = 0; $k < mt_rand(5, 15); $k++) {
-                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
-            }
-            $manager->persist($recipe);
-            $recipes[] = $recipe;
-        }
+        //     for ($k = 0; $k < mt_rand(5, 15); $k++) {
+        //         $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+        //     }
+        //     $manager->persist($recipe);
+        //     $recipes[] = $recipe;
+        // }
 
-        //marks
+        // //marks
 
-        foreach ($recipes as $recipe) {
-            $userschosen = [];
-            for ($i = 0; $i < mt_rand(3, 10); $i++) {
-                $userschosen[] = $users[mt_rand(0, count($users) - 1)];
-            }
-            $userschosen = array_unique($userschosen);
+        // foreach ($recipes as $recipe) {
+        //     $userschosen = [];
+        //     for ($i = 0; $i < mt_rand(3, 10); $i++) {
+        //         $userschosen[] = $users[mt_rand(0, count($users) - 1)];
+        //     }
+        //     $userschosen = array_unique($userschosen);
 
-            foreach ($userschosen as $userchosen) {
-                $mark = new Mark;
-                $mark->setMark(mt_rand(1, 5))
-                    ->setUser($userchosen)
-                    ->setRecipe($recipe);
-                $manager->persist($mark);
-            }
-        }
+        //     foreach ($userschosen as $userchosen) {
+        //         $mark = new Mark;
+        //         $mark->setMark(mt_rand(1, 5))
+        //             ->setUser($userchosen)
+        //             ->setRecipe($recipe);
+        //         $manager->persist($mark);
+        //     }
+        // }
 
-        //Contacts
+        // //Contacts
 
-        for ($i = 0; $i < 5; $i++) {
-            $contact = new Contact;
-            $contact->setFullName($this->faker->name())
-                ->setEmail($this->faker->email())
-                ->setSubject('Demande N° ' . $i)
-                ->setMessage($this->faker->text());
-            $manager->persist($contact);
-        }
+        // for ($i = 0; $i < 5; $i++) {
+        //     $contact = new Contact;
+        //     $contact->setFullName($this->faker->name())
+        //         ->setEmail($this->faker->email())
+        //         ->setSubject('Demande N° ' . $i)
+        //         ->setMessage($this->faker->text());
+        //     $manager->persist($contact);
+        // }
 
         $manager->flush();
     }
